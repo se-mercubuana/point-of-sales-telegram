2bef0142da29f5322ba08d40abde0bbc25869c72
Ramdani24 <faniramdani.24@gmail.com>
2019-08-27 19:45:25 +0700

    View on create Customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -30,7 +30,83 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function create()
+//    {
+//        return view('customer.create');
+//    }
 
+    {
+        $data = [
+            'customers' => Customer::all()
+        ];
+
+        return view('customer.create')->with($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function store(Request $request)
+    {
+        $code = self::createSlug(Customer::class, 'code', $request->code);
+
+        \App\Customer::insert([
+            'id' => Uuid::uuid(),
+            'code' => $code,
+            'name' => $request->name,
+            'no_telp' => $request->no_telp,
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now()
+        ]);
+
+
+        return redirect('/customer');
+    }
+
+
+    public static function createSlug($model, $field, $word)
+    {
+        $slug = Str::slug($word);
+
+        $slugs = $model::where($field, 'like', "{$slug}%")->get();
+
+        $slugs = $slugs->pluck('code')->toArray();
+        if (count($slugs) !== 0 and in_array($slug, $slugs)) {
+            $max = 0;
+
+            //keep incrementing $max until a space is found
+            while (in_array(($slug . '-' . ++$max), $slugs)) {
+            }
+
+            //update $slug with the appendage
+            $slug .= '-' . $max;
+        }
+
+        return $slug;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Customer $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return view('customer.detail');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Customer $customer
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Customer $customer)
     {
         //
