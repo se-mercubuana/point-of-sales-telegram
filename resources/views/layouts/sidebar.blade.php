b23230b84eff9bc3200c1f338e9e5f200d80a249
Gema Akbar <dityaksm21@gmail.com>
2019-08-30 01:48:49 +0700

    ustomer resolve

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -54,10 +54,6 @@
                                             href="/logout" aria-expanded="false"><i
                             class="mdi mdi-logout"></i><span class="hide-menu">Logout</span></a></li>
 
-                <li class="sidebar-item"><a class="sidebar-link waves-effect waves-dark sidebar-link"
-                                            href="/logout" aria-expanded="false"><i
-                                class="mdi mdi-view-dashboard"></i><span class="hide-menu">Logout</span></a></li>
-
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
